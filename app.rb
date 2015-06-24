273d6511dba0659a6c9fdae79b60661dbc785827
jordanrobarge <jordanrobarge@gmail.com>
2015-06-24 13:41:40 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
   get '/reddit' do
     # TODO: we can probably get the listings with something like:
     # JSON.parse(RestClient.get('http://reddit.com/.json'))
-    @listing = JSON.parse(RestClient.get('http://reddit.com/.json'))
+    @listings = JSON.parse(RestClient.get('http://reddit.com/.json'))
     #stuff1 = stuff[1]
     #stuff = stuff1[1]
     #stuff1=hash1["children"]
